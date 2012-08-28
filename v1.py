9bc51779ffddd3e854725157bb0251a9132655aa
Mark Lusby <mlusby@gmail.com>
2012-08-28 13:01:32 -0500

    Initial attempt to authenticate with v1

---
+++ v1.py
@@ -0,0 +1,21 @@
+import urllib2
+import base64
+import re
+
+url = "https://www10.v1host.com/ClickMotive/rest-1.v1/Data/Story?sel=Name,ToDo&where=ToDo='0'"
+request_object = urllib2.Request(url)
+base64string = base64.encodestring('%s:%s' % ('mlusby','Covax3141rb'))[:-1]
+authheader=  "Basic %s" % base64string
+request_object.add_header("Authorization", authheader)
+try:
+	doc = urllib2.urlopen(request_object)
+except IOError, e:
+	print "Error!"
+	if hasattr(e, 'code'):
+		print "ErrorCode: %s" % e.code
+		if e.code == 401:
+			authentication_string = e.headers['www-authenticate']
+
+print "Dumb Stuff"
+print doc.read()
+print "End Dumb Stuff"
