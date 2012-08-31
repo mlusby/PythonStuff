f1f0eefa550696d3c3135d806d1b3b3a38198ce2
Mark Lusby <mlusby@gmail.com>
2012-08-30 21:09:11 -0500

    B-0120 Next we have to figure out Git hooks

--- v1.py
+++ v1.py
@@ -2,6 +2,7 @@ import urllib2
 import base64
 import re
 
+#Just commenting
 url = "https://www10.v1host.com/ClickMotive/rest-1.v1/Data/Story?sel=Name,ToDo&where=ToDo='0'"
 request_object = urllib2.Request(url)
 base64string = base64.encodestring('%s:%s' % ('mlusby','FixThis'))[:-1]
